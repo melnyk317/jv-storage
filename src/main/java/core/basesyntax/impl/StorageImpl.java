344ad21d40d448584a3ad42a91789fc4b6d59c86
darina_melnyk <me7darina2008@gmail.com>
2026-03-26 15:19:34 +0200

    Task from README was done

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,44 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private K key;
+    private V value;
+
+    public StorageImpl(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public StorageImpl() {
+    }
+
+    StorageImpl[] storage = new StorageImpl[10];
+    int count = 0;
+
     @Override
     public void put(K key, V value) {
+        for (StorageImpl st : storage) {
+            if (st.key.equals(key)) {
+                st.value = value;
+                return;
+            }
+        }
+        storage[count] = new StorageImpl<K, V>(key, value);
+        count++;
     }
 
     @Override
     public V get(K key) {
+        for (StorageImpl st : storage) {
+            if (st.key.equals(key)) {
+                return (V) st.value;
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return count;
     }
 }
